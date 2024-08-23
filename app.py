c66a556cd105afbaf8436f2340e36196c7500980
devopsak1 <devopssystemserver@gmail.com>
2024-08-23 11:41:17 +0530

    Update data .....V3

--- app.py
+++ app.py
@@ -2,8 +2,8 @@ import streamlit as st
 import pandas as pd
 
 data = {
-    'Course Name': ['Devops', 'Azure', 'Snowflake', 'SQL'],
-    'Course Duration': ['30 Day', '35 Day', '40 Day', '30 Day']
+    'Course Name': ['Devops', 'Azure', 'Snowflake', 'SQL', 'Hadoop'],
+    'Course Duration': ['30 Day', '35 Day', '40 Day', '30 Day', '21 Day']
 }
 
 df = pd.DataFrame(data)
